f7ac64ac72be5b8ecf8453e42fbdafde75671fd1
Jaqueline Belagamba <jaqueline@elofy.com.br>
2020-08-05 21:46:03 -0300

    [atualiza]

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,23 @@
-const path = require('path');
+const path = require("path")
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js');
+  entry: path.resolve(__dirname, "src", "index.js"),
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [ // as in rules of engagement
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      }
+    ]
+  }
 }
